13b5b1314df176a0318c624dff803b5f5f8fc271
Vincent <3d314831994@gmail.com>
2020-02-07 18:41:25 +1100

    Pick weapon

---
+++ Assets/Scripts/PickWeapon.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickWeapon : MonoBehaviour
+{
+
+    public GameObject buttonE;
+    bool isShowingButton;
+    bool isHoldingWeapon;
+    [SerializeField]
+    GameObject weapon;
+
+    void Update()
+    {
+        if (isShowingButton)
+        {
+            if (Input.GetKeyDown(KeyCode.E) && weapon != null)
+            {
+                weapon.transform.SetParent(transform.GetChild(0));
+                weapon.transform.eulerAngles = new Vector3(-15, 110, -55);
+                isHoldingWeapon = true;
+            }
+            if (!isHoldingWeapon)
+            {
+                buttonE.SetActive(true);
+            }
+            else
+            {
+                buttonE.SetActive(false);
+            }
+        }
+        else {
+            buttonE.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Weapon") {
+            isShowingButton = true;
+            weapon = other.transform.parent.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Weapon")
+        {
+            isShowingButton = false;
+            weapon = null;
+        }
+    }
+}
