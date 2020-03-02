e75893e565817b5cea4af22b5855f219f60c38cb
Vincent <3d314831994@gmail.com>
2020-03-02 21:45:50 +1100

    Revised MainMenu
    when players click start, the text will fade out and light is flickering a while and turn off.

--- Assets/Scripts/MainMenu/UI_Button.cs
+++ Assets/Scripts/MainMenu/UI_Button.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class UI_Button : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,IPointerClickHandler
 {
-   
+    public UnityEvent onButtonClick;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
@@ -14,11 +15,21 @@ public class UI_Button : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        
+        OnClick();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         GetComponentInChildren<Animator>().SetBool("HighLight", false);
+    }
+
+    void OnClick() {
+        if (onButtonClick != null) {
+            onButtonClick.Invoke();
+        }
+    }
+
+    public void DissolveButton() {
+        GetComponentInChildren<Animator>().SetBool("Dissolve", true);
     }
 }
