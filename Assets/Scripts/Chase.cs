d183b67ab9dcffe44fddcf00a145b01133c5bf1f
chabeast <charbel_bouchmouny16@gotmail.com>
2020-03-02 20:23:10 +1100

    AI has patrol but attack gets stuck
    the hid the house so I can work with it

--- Assets/Scripts/Chase.cs
+++ Assets/Scripts/Chase.cs
@@ -6,7 +6,15 @@ public class Chase : MonoBehaviour
 {
 
     public Transform player;
+    public Transform head;
     static Animator anim;
+
+    string state = "patrol";
+    public GameObject[] waypoints;
+    int currentWP = 0;
+   public float rotSpeed = 0.2f;
+   public float speed = 1.5f;
+   public  float accuracyWP = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,32 +25,53 @@ public class Chase : MonoBehaviour
     void Update()
     {
         Vector3 direction = player.position - this.transform.position;
+        direction.y = 0;
         float angle = Vector3.Angle(direction, this.transform.forward);
-        if(Vector3.Distance(player.position, this.transform.position) < 10 && angle < 30)
-        {
-            
-            direction.y = 0;
 
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
-                                                        Quaternion.LookRotation(direction), 0.1f);
-            anim.SetBool("inIdle", false);
-            if(direction.magnitude > 5)
+        if (state == "patrol" && waypoints.Length > 0)
+        {
+            anim.SetBool("isWlaking", true);
+        if (Vector3.Distance(waypoints[currentWP].transform.position, transform.position) < accuracyWP)
             {
-                this.transform.Translate(0,0,0.05f);
-                anim.SetBool("isWalking", true);
-                anim.SetBool("isAttacking", false);
+                currentWP++;
+                if (currentWP >= waypoints.Length)
+                {
+                    currentWP = 0;
+                }
+            }
+
+
+            // rotate towards waypoint
+            direction = waypoints[currentWP].transform.position - transform.position;
+            this.transform.rotation = Quaternion.Slerp(transform.rotation,
+                               Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+            this.transform.Translate(0, 0, Time.deltaTime * speed);
+
+            if (Vector3.Distance(player.position, this.transform.position) < 10 && angle < 30 || state == "pursuing")
+            {
+               state = "pursuing";
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                                            Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime); ;
+
+                if (direction.magnitude > 5)
+                {
+                    this.transform.Translate(0, 0, Time.deltaTime * speed);
+                    anim.SetBool("isWalking", true);
+                    anim.SetBool("isAttacking", true);
+                }
+                else
+                {
+                    anim.SetBool("isAttacking", false);
+                    anim.SetBool("isWalking", false);
+                }
             }
             else
             {
-                anim.SetBool("isAttacking", true);
-                anim.SetBool("isWalking", false);
+                
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isAttacking", false);
+                state = "patrol";
             }
-        }
-        else
-        {
-            anim.SetBool("isIdle", true);
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isAttacking", false);
         }
     }
 }
