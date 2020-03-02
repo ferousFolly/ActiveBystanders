c81b7ddb8893d5f7c13bf9888cd8ca9925a64412
Vincent <3d314831994@gmail.com>
2020-03-02 11:39:31 +1100

    Add mainmenu and mouse over animation

---
+++ Assets/Scripts/MainMenu/UI_Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_Button : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,IPointerClickHandler
+{
+   
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponentInChildren<Animator>().SetBool("HighLight",true);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponentInChildren<Animator>().SetBool("HighLight", false);
+    }
+}
