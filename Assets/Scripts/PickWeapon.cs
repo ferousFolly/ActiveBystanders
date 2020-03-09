87ea6eb226a1d2890a1e22d465a9a29d0ab0161e
Vincent <3d314831994@gmail.com>
2020-03-10 10:52:39 +1100

    Lights man

--- Assets/Scripts/PickWeapon.cs
+++ Assets/Scripts/PickWeapon.cs
@@ -3,71 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PickWeapon : MonoBehaviour
+public class InteractiveAction : MonoBehaviour
 {
 
     public GameObject buttonE;
-    bool isShowingButton;
-    bool isHoldingWeapon;
-    [SerializeField]
-    GameObject weapon;
-
-    public Transform frontDoor;
 
     void Update()
     {
-        if (isShowingButton)
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1.2f))
         {
-            if (Input.GetKeyDown(KeyCode.E) && weapon != null)
+            switch (hit.collider.tag)
             {
-                weapon.transform.SetParent(transform.GetChild(0).GetChild(0));
-                weapon.transform.localPosition = Vector3.zero;
-                isHoldingWeapon = true;
-                isShowingButton = false;
-            }
-            if (!isHoldingWeapon)
-            {
-                buttonE.SetActive(true);
-            }
-            else
-            {
-                buttonE.SetActive(false);
+                case "Open":
+                    buttonE.SetActive(true);
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        hit.collider.GetComponentInParent<Animator>().SetBool("Open", true);
+                    }
+                    break;
             }
         }
         else {
             buttonE.SetActive(false);
-        }
-
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Open")
-        {
-            if (Input.GetKeyDown(KeyCode.E)) { 
-            other.GetComponent<Animator>().SetBool("Open", true);
-            }
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Weapon") {
-            isShowingButton = true;
-            weapon = other.transform.parent.gameObject;
-        }
-        if (other.tag == "Teleport")
-        {
-            transform.position = frontDoor.position;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Weapon")
-        {
-            isShowingButton = false;
-            weapon = null;
         }
     }
 }
