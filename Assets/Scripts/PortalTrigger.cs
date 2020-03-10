afc97b775974d38c1c9687d7e915102bf526d48f
Aydan H <tiddle97@gmail.com>
2020-03-10 16:59:55 +1100

    Intermediary commit
    Not finished

---
+++ Assets/Scripts/PortalTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTrigger : MonoBehaviour {
+    public Transform triggerBox;
+    public GameObject portalCamera;
+    public GameObject portal;
+
+    void OnCollisionEnter() {
+        if (!portal.activeInHierarchy) {
+            portal.SetActive(true);
+            portalCamera.SetActive(true);
+        }
+    }
+}
