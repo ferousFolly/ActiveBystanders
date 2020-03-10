950c0503206b33e78fc70efd21f3268eb750fedb
charbel <charbel_bouchmouny16@hotmail.com>
2020-03-10 16:02:52 +1100

    PLAYER death
    needs work

---
+++ Assets/Scripts/Charbel/AI_Damage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Damage : MonoBehaviour
+{
+
+    public float damageAmount = 35;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag =="Player") {
+            other.GetComponent<PlayerDying>().TakeDamage(damageAmount);
+        }
+    }
+}
