bc62c066f8ee718660c1df499fe31eacbd223dc3
chabeast <charbel_bouchmouny16@gotmail.com>
2020-03-29 21:24:15 +1100

    added a room change script and changed the layout
    we might do this in every room so the rituals can be hard to find

---
+++ Assets/Scripts/Charbel/RoomChange.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomChange : MonoBehaviour
+{
+    public  Transform player;
+    public GameObject SwapTrigger;
+    public bool _LivingRoom = false;
+    public bool LivingRoomSwap = false;
+
+
+    // Start is called before the first frame update
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (SwapTrigger)
+        {
+            _LivingRoom = true;
+        }
+        else
+            LivingRoomSwap = false;
+
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (SwapTrigger)
+        {
+            LivingRoomSwap = true;
+        }
+        else
+            _LivingRoom = false;
+       
+        
+    }
+}
