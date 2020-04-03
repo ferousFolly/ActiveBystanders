d5f94f8eddb24e863df33ce22ddcc88f389bfe2f
Arjun <1016487@student.sae.edu.au>
2020-04-03 15:59:11 +1100

    Fence & Garden Added

---
+++ Assets/Export Cutscene/SceneSequence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSequence : MonoBehaviour
+{
+    public GameObject Cam1;
+    public GameObject Cam2;
+    public GameObject Cam3;
+    public GameObject Cam4;
+
+    void Start()
+    {
+        StartCoroutine(TheSequence());
+    }
+
+    IEnumerator TheSequence ()
+    {
+        Cam1.SetActive(true);
+        yield return new WaitForSeconds(4);
+        Cam2.SetActive(true);
+        Cam1.SetActive(false);
+        yield return new WaitForSeconds(4);
+        Cam3.SetActive(true);
+        Cam2.SetActive(false);
+        yield return new WaitForSeconds(4);
+        Cam4.SetActive(true);
+        Cam3.SetActive(false);
+    }
+}
