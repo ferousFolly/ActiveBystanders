078c041c447735a6aa35dc484efbdfa1ccad3fa3
chabeast <charbel_bouchmouny16@gotmail.com>
2020-04-20 16:34:56 +1000

    Merge with me

--- Assets/Scripts/Charbel/RoomChange.cs
+++ Assets/Scripts/Charbel/RoomChange.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class RoomSetting {
+public class RoomSetting
+{
     public string name;
     public GameObject originalRoom;
     public GameObject swapRoom;
@@ -13,32 +14,6 @@ public class RoomChange : MonoBehaviour
 {
     Transform player;
     public List<RoomSetting> roomSettings;
-
-
-    public GameObject LivingRoom;
-    public GameObject LivingRoomSwap;
-
-    public GameObject Bathroom;
-    public GameObject BathroomSwap;
-
-
-    public GameObject Basement;
-    public GameObject BasementSwap;
-
-
-    public GameObject SmallBedroom;
-    public GameObject SmallBedroomSwap;
-
-    public GameObject Dinningroom;
-    public GameObject DinningroomSwap;
-
-    public GameObject Mainbedroom;
-    public GameObject MainbedroomSwap;
-
-    public GameObject Demon;
-    public GameObject DemonSwap;
-
-
 
     BoxCollider collider;
 
@@ -81,36 +56,17 @@ public class RoomChange : MonoBehaviour
                 roomSettings[i].swapRoom.SetActive(!roomSettings[i].originalRoom.activeInHierarchy);
             }
 
-
-            Basement.SetActive(BasementSwap.activeInHierarchy);
-            BasementSwap.SetActive(!Basement.activeInHierarchy);
-
-            SmallBedroom.SetActive(SmallBedroomSwap.activeInHierarchy);
-            SmallBedroomSwap.SetActive(!SmallBedroom.activeInHierarchy);
-
-            Dinningroom.SetActive(DinningroomSwap.activeInHierarchy);
-            DinningroomSwap.SetActive(!Dinningroom.activeInHierarchy);
-
-            Mainbedroom.SetActive(MainbedroomSwap.activeInHierarchy);
-            MainbedroomSwap.SetActive(!Mainbedroom.activeInHierarchy);
-
-
-            Demon.SetActive(DemonSwap.activeInHierarchy);
-            DemonSwap.SetActive(!Demon.activeInHierarchy);
-        }
-
-        if (gameObject.name != "LilyRoomTrigger")
-        {
-            if (GameEventObserver.i.IsDialougeCompleted())
+            if (gameObject.name != "LilyRoomTrigger")
             {
-                GameEventObserver.i.isRoomChange = true;
+                if (GameEventObserver.i.IsDialougeCompleted())
+                {
+                    GameEventObserver.i.isRoomChange = true;
+                }
             }
-        }
-        else
-        {
-            GameEventObserver.i.isEnterLilyRoom = true;
+            else
+            {
+                GameEventObserver.i.isEnterLilyRoom = true;
+            }
         }
     }
 }
-
-  
