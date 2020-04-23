cb5a21ae5500941e2111c80fe300cb7212250851
Arjun <1016487@student.sae.edu.au>
2020-04-23 20:36:00 +1000

    Added Cheat menu and changed cutscene position

---
+++ Assets/OpenTerminal/Sample/RotateCube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    private bool stop;
+    void Update()
+    {
+        if (!stop)
+            transform.Rotate(new Vector3(1, 1, 1));
+    }
+
+
+    [Command("stop-cube", "stops the cube from rotating")]
+    public void StopCube()
+    {
+        stop = true;
+    }
+
+
+    [Command("rotate-cube", "rotates the cube")]
+    public void RotateTheCube()
+    {
+        stop = false;
+    }
+
+    [Command("move-cube", "move-cube(x,y,z) Moves the cube")]
+    public void Move(float x, float y, float z)
+    {
+        transform.position = new Vector3(x, y, z);
+    }
+}
